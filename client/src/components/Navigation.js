9657adaac9fa1629991044bed5a84e0a7b0c102f
Tim McCann <timothysmccann@gmail.com>
2024-03-11 16:48:35 -0400

    working on frontend

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,7 +1,61 @@
+import { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
 
+function Navigation({updateUser}){
+    const history = useHistory()
 
-function Navigation(){
+    useEffect(() => {
+        fetchUserData()
+    }, [])
 
+    const fetchUserData = () => {
+        fetch('/users')
+        .then(resp => {
+            if (resp.ok){
+                resp.json().then(user => {
+                    updateUser(user)
+                })
+            } else {
+                updateUser(null)
+            }
+        })
+    }
+
+    const handleLogout = () => {
+        fetch('/logout', {
+            method: 'DELETE',
+        })
+     .then(resp => {
+        if (resp.ok){
+            fetchUserData(null)
+            history.push('/')
+        }
+     })
+    }
+
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/locations">Locations</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                    <button onClick={handleLogout}>Logout</button>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navigation
