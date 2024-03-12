5d4192e57d99697bb8717c95e185191409e98ec3
Tim McCann <timothysmccann@gmail.com>
2024-03-12 11:08:06 -0400

    fixed up locations component to have areacontainer as the child of it

--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,38 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import AreaContainer from './AreaContainer';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 function Locations({ areas }) {
+    const [map, setMap] = useState(null);
     const [locations, setLocations] = useState([]);
 
     useEffect(() => {
         mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
-        const map = new mapboxgl.Map({
+        const mapInstance = new mapboxgl.Map({
             container: 'map', // container ID
             style: 'mapbox://styles/mapbox/streets-v11', // style URL
             center: [-74.5, 40], // starting position [lng, lat]
             zoom: 9 // starting zoom
         });
-        return () => map.remove();
+        setMap(mapInstance);
+
+        return () => mapInstance.remove();
     }, []);
 
     useEffect(() => {
-        if (areas.length > 0) {
+        if (map && areas.length > 0) {
             setLocations(areas);
         }
-    }, [areas]);
+    }, [map, areas]);
 
     return (
         <div className="locations-container">
             <div className="locations-list">
                 <h2>Locations</h2>
                 <ul>
-                    {locations.map(location => (
-                        <li key={location.id}>
-                            <h3>{location.name}</h3>
-                            <p>{location.address}</p>
-                        </li>
+                    {areas.map(area => (
+                        <AreaContainer key={area.id} area={area} />
                     ))}
                 </ul>
             </div>
