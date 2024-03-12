c9ec961dacff9857573fbff6600d0ca0d8e55013
Tim McCann <timothysmccann@gmail.com>
2024-03-12 12:01:52 -0400

    working on reviews

--- client/src/components/ReviewsContainer.js
+++ client/src/components/ReviewsContainer.js
@@ -1,10 +1,19 @@
 import { useHistory } from 'react-router-dom'
 import {useEffect, useState} from 'react'
+import Review from './Review'
 
 function ReviewsContainer(){
     const [reviews, setReviews] = useState([])
     const [user, setUser] = useState(null)
+    const [reviewToEdit, setReviewToEdit] = useState([])
     const history = useHistory()
+
+    const addReview = (review) => setReviews(current => [...current, review])
+    const deleteReview = (deleted_review) => setReviews(reviews => reviews.filter((review) => review.id !== deleted_review.id))
+    const updateReview = (review) => {
+        setReviewToEdit(review)
+        history.push('/reviews')
+    }
 
     function handleDelete(review){
         fetch(`/climbing_areas/${review.id}`, {
@@ -16,6 +25,34 @@ function ReviewsContainer(){
             }
         })
     }
+
+    function handleAddReview(review, rating, comment, date) {
+        fetch('/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                review: review,
+                rating: rating,
+                comment: comment,
+                date : date
+            })
+        }).then (resp => {
+            if (resp.ok){
+                addReview(review)
+                history.push(`/locations/${.id}`)
+            }
+        })
+    }
+
+    return (
+        <>
+            {reviews.map((review) => (
+                <Review key={review.id} review={review} handleDelete={handleDelete} updateReview={updateReview} handleAddReview={handleAddReview}/>
+            ))}
+        </>
+    )
 
 }
 
